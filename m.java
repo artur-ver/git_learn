b5eb8780ae4013c2d4f319740724b6639c9e74f3
artur-ver <arturverbovsuk@gmail.com>
2025-08-31 15:34:34 +0200

    python

--- m.java
+++
@@ -1,9 +0,0 @@
-let a = 10;
-let b = 20;
-let c = a + b;
-console.log(c);
-// Output: 30
-
-let d = a * b;
-console.log(d);
-// Output: 200
--- m.py
+++ m.py
@@ -1,10 +1,16 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
-print("Done")
+print("hello")
+print("world")
+print("!")
+
+# m.py
+print("hello")
+print("world")
+print("!")
+print("!!!")
+print("!!!!")
+print("!!!!!")
+print("!!!!!!")
+print("!!!!!!!")
+print("!!!!!!!!")
+
+print("!!!!!!!!!")
